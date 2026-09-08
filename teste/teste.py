0750e7b64ae01f75c398167ab387b6f668550695
RafaelEvangelista1b <evangelistafreitasrafael@gmail.com>
2026-09-08 15:44:07 -0300

    Add files via upload

---
+++ teste/teste.py
@@ -0,0 +1,13 @@
+email = 'USUARIO@GMAIL.COM'
+print(email)
+print(email.lower()) # lower deixa o texto em letra minuscula
+
+objeto = " lapis "
+print(objeto)
+print(objeto.strip()) # strip tira os espaços
+
+palavra = "Carta"
+print(palavra)
+print(len(palavra)) # tamanho
+
+
